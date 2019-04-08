c191a99d5c2b9231bd5cc2e667812c2e6157ed4f
Alecx Moritz <alecxmoritz7@gmail.com>
2019-04-08 14:32:27 -0400

    added day two solution

---
+++ 2-blueBadge/week06/server-challenges-solutions/06.02.js
@@ -0,0 +1,68 @@
+// Create, Update and Delete
+
+// * Route needs to be added to index.js
+
+
+
+var express = require('express');
+var router = express.Router();
+var sequelize = require('../db');
+var Animal = sequelize.import('../models/animal');
+
+// Bronze
+router.post('/create', (req, res) => {
+    Animal.create({
+        name: req.body.name,
+        legNumber: req.body.legNumber,
+        predator: req.body.predator
+    })
+        .then(
+            createSuccess = animal => {
+                res.status(200).json(animal);
+            },
+
+            createFail = err => {
+                res.status(500).json({ error: err });
+            }
+        );
+});
+
+// Silver
+router.delete('/:id', (req, res) => {
+    Animal.destroy({ where: { id: req.params.id } })
+        .then(
+            deleteSuccess = recordsChanged => {
+                res.status(200).json({ message: `${recordsChanged} record(s) deleted.` })
+            },
+
+            deleteFail = err => {
+                res.status(500).json({ message: 'Failed to delete', error: err })
+            }
+        );
+});
+
+// Gold
+router.put('/:id', (req, res) => {
+    Animal.update({
+        name: req.body.name,
+        legNumber: req.body.legNumber,
+        predator: req.body.predator
+    },
+        {
+            where: {
+                id : req.params.id
+            }
+        })
+        .then(
+            updateSuccess = recordsChanged => {
+                res.status(200).json({ message: 'Animal Updated' })
+            },
+
+            updateFail = err => {
+                res.status(500).json({ message: 'Update failed', error: err })
+            }
+        )
+})
+
+
+module.exports = router;
